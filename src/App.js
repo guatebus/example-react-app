8008ad091b3090cd28d7668c36d1f85af86b4293
Alejandro Bustamante <alejandro.bustamante@scmspain.com>
2017-03-10 10:26:00 +0100

    created two custom components with props use

--- src/App.js
+++ src/App.js
@@ -2,17 +2,33 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class Monger extends Component {
+  render() {
+    console.log(this.props)
+    return <h2>{this.props.saludo} {this.props.literal}</h2>
+  }
+}
+
+class Paragraph extends Component {
+  render() {
+    return <p>{this.props.print}</p>
+  }
+}
+
 class App extends Component {
   render() {
+    const saludo = 'yuuuhuuu!!'
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>WQuiero ser un monki core monki</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Monger saludo={saludo} literal='pipo' gender='neutral'/>
+        <Monger saludo={saludo} literal='pipa' gender='neutral'/>
+        <Monger saludo='yumyum' literal='yuli' age={33}/>
+
+        <Paragraph print='Lorem ipsum monki trolling'/>
       </div>
     );
   }
